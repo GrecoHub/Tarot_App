dbb7e65d0abbd23ee866cc80a449a01ce52701e9
Stephen Greco <email@example.com>
2019-04-13 09:11:24 -0400

    Adding Mokey link

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,3 @@
-// Step 1  Create action that fires the pile using console.log
-// Step 2 link the action with the flipped cards
-// step 3 grab card names and turn them into strings
-
 //Card Variables
 
 let numCards = 10;
@@ -15,41 +11,37 @@ let flippedCard1 = document.getElementById("image1");
 let flippedCard2 = document.getElementById("image2");
 let flippedCard3 = document.getElementById("image3");
 
-let cards = () => {
-  fetch("https://www.mocky.io/v2/5ca7a19f520000b50b97b681")
-    .then(response => {
-      return response.json();
-    })
-    .then(function(myJson) {
-      console.log(JSON.stringify(myJson));
-    });
-};
+fetch("http://www.mocky.io/v2/5cb1dc1333000073275720f4")
+  .then(response => {
+    return response.json();
+  })
+  .then(function(cards) {
+    let layout = () => {
+      // Read up on Sort
+      let keys = Object.keys(cards).sort(() => 0.5 - Math.random());
+      flippedCard1.src = cards[keys[0]].src;
+      flippedCard2.src = cards[keys[1]].src;
+      flippedCard3.src = cards[keys[2]].src;
+      flippedCard1.onmouseover = () => {
+        cardDescription.innerHTML = cards[keys[0]].description;
+      };
 
-let layout = () => {
-  // Read up on Sort
-  let keys = Object.keys(cards).sort(() => 0.5 - Math.random());
-  flippedCard1.src = cards[keys[0]].src;
-  flippedCard2.src = cards[keys[1]].src;
-  flippedCard3.src = cards[keys[2]].src;
-  flippedCard1.onmouseover = () => {
-    cardDescription.innerHTML = cards[keys[0]].description;
-  };
+      flippedCard2.onmouseover = () => {
+        cardDescription.innerHTML = cards[keys[1]].description;
+      };
 
-  flippedCard2.onmouseover = () => {
-    cardDescription.innerHTML = cards[keys[1]].description;
-  };
+      flippedCard3.onmouseover = () => {
+        cardDescription.innerHTML = cards[keys[2]].description;
+      };
+    };
 
-  flippedCard3.onmouseover = () => {
-    cardDescription.innerHTML = cards[keys[2]].description;
-  };
-};
+    //Command + D selects duplicates
 
-//Command + D selects duplicates
-
-pile.onclick = () => {
-  layout();
-  cardDescription.innerHTML = "Hover over each card to find out more!";
-};
+    pile.onclick = () => {
+      layout();
+      cardDescription.innerHTML = "Hover over each card to find out more!";
+    };
+  });
 
 // When hovering over card in layout, key.description appears in cardDescription.innerHTML
 
@@ -88,3 +80,6 @@ pile.onclick = () => {
 // Cards object can be found at: http://www.mocky.io/v2/5cabecdc300000a019103287
 // In brackets: http://www.mocky.io/v2/5cabedf3300000781710328a
 // Dave's original Link: https://www.mocky.io/v2/5ca7a19f520000b50b97b681
+
+// Terminal command `python - m SimpleHTTPServer`
+// url = http://localhost:8000/
